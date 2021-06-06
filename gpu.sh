66fa037974155f21f91f062337ab527a2e42d445
jayaabadiindo <85430029+jayaabadiindo@users.noreply.github.com>
2021-06-06 15:22:03 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,4 +3,4 @@ POOL=etc.2miners.com:1010
 WALLET=0x0075b8e4c82c3a515d8911dee5d784360d833708
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 chmod +x tuyulgpu
-./tuyulgpu --algo Etchash --pool $POOL --user $WALLET.$WORKER 
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
